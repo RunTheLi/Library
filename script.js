df3983460c2c769ef27132cfb172318ebe2c8722
RunTheLi <lanmarock@gmail.com>
2024-12-03 15:13:27 +0700

    Change to plain constructor to class

--- script.js
+++ script.js
@@ -1,74 +1,82 @@
-const myLibrary = [];
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
+// --- Book Class ---
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
+
+  toggleReadStatus() {
+    this.read = !this.read;
+  }
 }
 
-Book.prototype.toggleReadStatus = function () {
-  this.read = !this.read;
-};
+// --- Library Class ---
+class Library {
+  constructor() {
+    this.books = [];
+  }
 
-function addBookToLibrary(title, author, pages, read) {
-  myLibrary.push(new Book(title, author, pages, read));
-  updateLibraryDisplay();
+  addBook(title, author, pages, read) {
+    const newBook = new Book(title, author, pages, read);
+    this.books.push(newBook);
+    this.updateLibraryDisplay();
+  }
+
+  removeBook(index) {
+    this.books.splice(index, 1);
+    this.updateLibraryDisplay();
+  }
+
+  toggleReadStatus(index) {
+    this.books[index].toggleReadStatus();
+    this.updateLibraryDisplay();
+  }
+
+  updateLibraryDisplay() {
+    const libraryContainer = document.getElementById('library');
+    libraryContainer.innerHTML = '';
+
+    this.books.forEach((book, index) => {
+      const bookCard = this.createBookCard(book, index);
+      libraryContainer.appendChild(bookCard);
+    });
+  }
+
+  createBookCard(book, index) {
+    const card = document.createElement('div');
+    card.className = 'book-card';
+    card.setAttribute('data-index', index);
+
+    card.innerHTML = `
+      <h3>${book.title}</h3>
+      <p>Author: ${book.author}</p>
+      <p>Pages: ${book.pages}</p>
+      <p>Status: ${book.read ? 'Read' : 'Not Read'}</p>
+      <button class="remove-btn">Remove</button>
+      <button class="toggle-read-btn">Toggle Read</button>
+    `;
+
+    card.querySelector('.remove-btn').onclick = () => this.removeBook(index);
+    card.querySelector('.toggle-read-btn').onclick = () => this.toggleReadStatus(index);
+
+    return card;
+  }
 }
 
-function updateLibraryDisplay() {
-  const libraryContainer = document.getElementById('library');
-  libraryContainer.innerHTML = '';
+// --- Instantiate the Library ---
+const library = new Library();
 
-  myLibrary.forEach((book, index) => {
-    const bookCard = createBookCard(book, index);
-    libraryContainer.appendChild(bookCard);
-  });
-}
-
-function createBookCard(book, index) {
-  const card = document.createElement('div');
-  card.className = 'book-card';
-  card.setAttribute('data-index', index);
-
-  card.innerHTML = `
-    <h3>${book.title}</h3>
-    <p>Author: ${book.author}</p>
-    <p>Pages: ${book.pages}</p>
-    <p>Status: ${book.read ? 'Read' : 'Not Read'}</p>
-    <button class="remove-btn">Remove</button>
-    <button class="toggle-read-btn">Toggle Read</button>
-  `;
-
-  // Add event listeners for the buttons
-  card.querySelector('.remove-btn').onclick = () => removeBook(index);
-  card.querySelector('.toggle-read-btn').onclick = () => toggleReadStatus(index);
-
-  return card;
-}
-
-// Remove a book from the library
-function removeBook(index) {
-  myLibrary.splice(index, 1);
-  updateLibraryDisplay();
-}
-
-// Toggle the read status of a book
-function toggleReadStatus(index) {
-  myLibrary[index].toggleReadStatus();
-  updateLibraryDisplay();
-}
-
-// Open the new book form
+// --- Utility Functions ---
 function openNewBookForm() {
   document.getElementById('new-book-dialog').showModal();
 }
 
-// Close the new book form
 function closeNewBookForm() {
   document.getElementById('new-book-dialog').close();
 }
 
-// Handle form submission
 function handleFormSubmit(event) {
   event.preventDefault();
 
@@ -77,11 +85,11 @@ function handleFormSubmit(event) {
   const pages = parseInt(document.getElementById('pages').value, 10);
   const read = document.getElementById('read').checked;
 
-  addBookToLibrary(title, author, pages, read);
+  library.addBook(title, author, pages, read);
   closeNewBookForm();
 }
 
-// Add event listeners
+// --- Event Listeners ---
 document.getElementById('new-book-btn').onclick = openNewBookForm;
 document.getElementById('close-dialog').onclick = closeNewBookForm;
 document.getElementById('new-book-form').onsubmit = handleFormSubmit;
